25629425e90227739708e3047b9f2709501e26ae
linxin <linxin8@outlook.com>
2020-10-07 21:32:01 +0800

    use internal buffer

--- src/file.cpp
+++ src/file.cpp
@@ -1,15 +1,30 @@
 #include "file.h"
 #include <cassert>
+#include <cstdlib>
+#include <sys/stat.h>
 
-WriteOnlyFile::WriteOnlyFile(const char* path) : _file{fopen(path, "w")}, _buffer{}, _byteWrited{0}
+WriteOnlyFile::WriteOnlyFile(const char* path) : _file{fopen(path, "w")}, _byteWrited{0}
 {
     assert(_file);
-    setbuffer(_file, _buffer, sizeof(_buffer));
+    struct stat stats;
+    if (fstat(fileno(_file), &stats) == -1)
+    {  // POSIX only
+        std::perror("fstat");
+        std::abort();
+    }
+    if (std::setvbuf(_file, NULL, _IOFBF, stats.st_blksize) != 0)
+    {
+        std::perror("setvbuf failed");  // POSIX version sets errno
+        std::abort();
+    }
+    // not need alloc buffer manully
+    // setbuffer(_file, _buffer, sizeof(_buffer));
 }
 
 WriteOnlyFile::~WriteOnlyFile()
 {
     fclose(_file);  // fclose will automatically flush
+    // delete _buffer;
 }
 
 void WriteOnlyFile::append(const char* data, size_t size)
